210f9ddbfd74c21be66f31024a1e9999ec659d13
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-01-31 11:00:48 +0100

    Add Prometheus Metrics exporter

--- running-venice-no-docker/run_controller.sh
+++ running-venice-no-docker/run_controller.sh
@@ -2,4 +2,5 @@
 
 CONFIGDIR=${1:-config-no-tls}
 echo "Using configuration in $CONFIGDIR"
-java -Djavax.net.debug=all  -Djava.net.preferIPv4Stack=true -jar bin/venice-controller-all.jar $CONFIGDIR/cluster.properties $CONFIGDIR/controller.properties
+java -javaagent:./jmx_prometheus_javaagent-0.17.2.jar=9999:prometheus_config.yaml -Djava.net.preferIPv4Stack=true -jar bin/venice-controller-all.jar $CONFIGDIR/cluster.properties $CONFIGDIR/controller.properties
+
--- running-venice-no-docker/run_server.sh
+++ running-venice-no-docker/run_server.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 CONFIGDIR=${1:-config-no-tls}
 echo "Using configuration in $CONFIGDIR"
-java  -Djava.net.preferIPv4Stack=true -jar bin/venice-server-all.jar $CONFIGDIR
+java -javaagent:./jmx_prometheus_javaagent-0.17.2.jar=9998:prometheus_config.yaml  -Djava.net.preferIPv4Stack=true -jar bin/venice-server-all.jar $CONFIGDIR
